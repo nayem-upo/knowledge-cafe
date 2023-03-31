e6ac8561a297d84fab4af10f45e88e8fe6d7be3c
programmer-nayem <nayemislam0181@gmail.com>
2023-03-31 19:59:19 +0600

    mobile responsive done

--- src/Components/Bonus/Bonus.jsx
+++ src/Components/Bonus/Bonus.jsx
@@ -8,7 +8,7 @@ class Bonus extends Component {
             <div className="bonus-main">
                 <div>
                     <h1 className="title">Answers to Assignment No. 8</h1>
-                    <div>
+                    <div className="answers-main">
                         <h4 className="answers"><small>1. Props vs state:</small> Props allows passing data which are read-only components. But State is an updatable structure that can change over time.</h4>
                         <h4 className="answers"><small>2. How does useState work?</small> When we call the useState hook inside a function, it returns an array with two values. The first value is the current value and the second value is a function. We can update the first value by using the function.</h4>
                         <h4 className="answers"><small>3. Purpose of useEffect other than fetching data.</small> The main purpose of useEffect other than fetching data is it allows us to perform side effects in our components.</h4>
